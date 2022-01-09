33cb6aeef8f32d9811078bc406b0b0bc8dfa8826
ducphamle2 <ducphamle212@gmail.com>
2022-01-09 11:31:01 +0700

    init deno scripts

---
+++ src/pricefeed/coingecko.js
@@ -0,0 +1,98 @@
+import { createRequire } from "https://deno.land/std@0.120.0/node/module.ts";
+console.log("import meta url: ", import.meta.url);
+const nodeRequire = createRequire(import.meta.url);
+const https = nodeRequire("https");
+
+const httpGet = (url) => {
+    return new Promise((resolve, reject) => {
+        https
+            .get(url, (res) => {
+                let body = "";
+                res.on("data", (chunk) => (body += chunk));
+                res.on("end", () => resolve(body));
+            })
+            .on("error", reject);
+    });
+};
+
+const getPrice = async (url, count) => {
+    try {
+        if (count > 10) return null;
+        const result = await httpGet(url);
+        const resultObj = JSON.parse(result);
+        return resultObj;
+    } catch (error) {
+        setTimeout(getPrice, 5000, url, count + 1);
+    }
+};
+
+const main = async (symbols) => {
+    const responses = [];
+    const listSymbols = JSON.parse(symbols);
+
+    const urls = listSymbols.map(
+        (symbol) =>
+            `https://api.coingecko.com/api/v3/simple/price?ids=${symbol}&vs_currencies=usd`,
+    );
+
+    const mappingSymbols = [
+        "BTC",
+        "ETH",
+        "BNB",
+        "XRP",
+        "DOGE",
+        "USDT",
+        "LINK",
+        "UNI",
+        "USDC",
+        "BUSD",
+        "ORAI",
+        "DAI",
+        "SOL",
+        "MATIC",
+        "SUSHI",
+        "DOT",
+        "LUNA",
+        "ICP",
+        "XLM",
+        "ATOM",
+        "AAVE",
+        "THETA",
+        "EOS",
+        "CAKE",
+        "AXS",
+        "ALGO",
+        "MKR",
+        "KSM",
+        "XTZ",
+        "FIL",
+        "AMP ",
+        "RUNE",
+        "COMP",
+        "OHM",
+        "TIME",
+        "MIM",
+        "SPELL",
+        "ICE",
+        "GALA",
+        "MANA",
+        "ENJ",
+        "SAND",
+    ];
+
+    for (let i = 0; i < urls.length; i++) {
+        const resultObj = await getPrice(urls[i]);
+        if (resultObj) {
+            if (listSymbols[i] in resultObj) {
+                let priceUsd = parseFloat(resultObj[listSymbols[i]].usd).toFixed(8);
+                responses.push({
+                    name: mappingSymbols[i],
+                    prices: [priceUsd.toString()],
+                });
+            }
+        }
+    }
+    console.log(JSON.stringify(responses))
+};
+
+main(...process.argv.slice(2));
